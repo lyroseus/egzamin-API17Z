308cb1605605fd671473c1e449af2ad4c434ad40
Katarzyna Burek <126965358+lyroseus@users.noreply.github.com>
2023-03-27 21:10:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const titleElement = document.getElementById("title");
+console.log(titleElement);
+titleElement.textContent = "Artykuł testowy: Katarzyna Burek";
+
+const paragraphElement = document.getElementById ("paragraph");
+console.log (paragraphElement);
+paragraphElement.textContent = "Lorem ipsum dolor sit amet, consectetur adipisicing vel, sit amet consectetur adipisicing elit. Atque, beatae? Magni, architecto facere eos aperiam deserunt non incidunt facilis quititsoril."
+
